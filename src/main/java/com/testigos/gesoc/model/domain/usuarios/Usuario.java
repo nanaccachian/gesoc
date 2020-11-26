a6e95ab482fa9e30740727cc052c820c4ad05191
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2020-11-25 21:58:45 -0300

    Pequenios cambios en usuario

--- src/main/java/com/testigos/gesoc/model/domain/usuarios/Usuario.java
+++ src/main/java/com/testigos/gesoc/model/domain/usuarios/Usuario.java
@@ -8,10 +8,14 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.testigos.gesoc.model.domain.egresos.EgresoConPresupuestos;
+import com.testigos.gesoc.model.domain.entidades.Entidad;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -30,6 +34,10 @@ public class Usuario {
     @Column
     private @Getter @Setter String permisos;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "entidad_id")
+    private @Getter @Setter Entidad entidad;
+
     @Column
     private @Getter @Setter String name;
 
@@ -39,18 +47,18 @@ public class Usuario {
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private @Getter @Setter List<EgresoConPresupuestos> egresosConPresupuestos = new ArrayList<>();
 
-    public Usuario(String username, String password, String permisos, String name, String surname) {
+    public Usuario(String username, String password, String permisos, String name, String surname, Entidad entidad) {
         this.username = username;
         this.password = password;
         this.permisos = permisos;
         this.name = name;
         this.surname = surname;
+        this.entidad = entidad;
     }
 
     @Override
     public String toString() {
-        return "Usuario [egresosConPresupuestos=" + egresosConPresupuestos + ", name=" + name + ", password=" + password
-                + ", permisos=" + permisos + ", surname=" + surname + ", username=" + username + "]";
+        return "Usuario [entidad=" + entidad + ", name=" + name + ", password=" + password + ", permisos=" + permisos
+                + ", surname=" + surname + ", username=" + username + "]";
     }
-
 }
